98ef99616e148bb9745d31002e4d9c85a1061357
Mark Redeman <markredeman@gmail.com>
2015-12-25 23:18:42 +0100

    Vectorize nonuniform central first order method

--- +discretizations/+first_derivative/nonuniform_central_first_order.m
+++ +discretizations/+first_derivative/nonuniform_central_first_order.m
@@ -1,14 +1,7 @@
 %% nonuniform_first_order:
 function [A] = nonuniform_central_first_order(x)
     N = length(x);
-    A = sparse(N, N);
-    hm = x(2) - x(1);
 
-    parfor j = 2 : N - 1
-        hp = x(j + 1) - x(j);
-        h2 = hp + hm;
-        A(j, j - 1) = 1 / h2;
-        A(j, 1 + j) = - 1 / h2;
-        hm = hp;
-    end
+    h = 1 ./ [1, x(3 : end) - x(1 : end - 2), 1];
+    A = spdiags([ - [h(2 : end), 1]', [1, h(1 : end - 1)]'], [- 1, 1], N, N);
 end
