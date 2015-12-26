99263bef6573efa8a1be64763c8156b6486ab7ad
Mark Redeman <markredeman@gmail.com>
2015-12-26 12:51:16 +0100

    Add test checking that nonuniform central reverts to uniform central

---
+++ tests/first_derivative/Nonuniform_Central_First_Order_Test.m
@@ -0,0 +1,27 @@
+classdef Nonuniform_Central_First_Order_Test < matlab.unittest.TestCase
+
+    properties (TestParameter)
+        N = {4, 8, 16, 32, 64, 128, 256, 512, 1024};
+    end
+
+    methods(Test, TestTags={'Small'})
+        %% test_nonuniform_equals_central_for_uniform_grid:
+        function test_nonuniform_equals_central_for_uniform_grid(t, N)
+            import discretizations.first_derivative.nonuniform_central_first_order;
+            import discretizations.first_derivative.central_second_order;
+
+            x = linspace(0, 1, N);
+            A = nonuniform_central_first_order(x);
+            C = central_second_order(x);
+
+            t.assertEqual(A(2 : end - 1, 2 : end - 1), C(2 : end - 1, 2 : end -1), 'AbsTol', eps(200 * N));
+        end
+    end
+
+    methods (Test, TestTags={'Medium'})
+        %% test_consistency_of_second_order_central:
+        function test_consistency_of_second_order_central(t)
+
+        end
+    end
+end
